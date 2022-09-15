8dc29f81008f6249055443db2f4ca022c0d2ae5f
yrom1 <ryanm.inbox@gmail.com>
2022-09-15 13:55:55 -0400

    Update tests to new MutableMapping API

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,5 +5,5 @@ from magical_cloud_dictionary import Magic
 
 def test_put_get():
     mp = Magic("test")
-    mp.put("answer", 42)
-    assert mp.get("answer") == 42
+    mp["answer"] = 42
+    assert mp["answer"] == 42
